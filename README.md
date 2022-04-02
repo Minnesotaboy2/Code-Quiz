a9d1a90af703adc965b5f45db5df71af38fc2690
Andrew Martinek <andrewdmartinek@gmail.com>
2022-04-02 17:36:21 -0500

    buttons now display correct or incorrect, with incorrect answers taking time off the countdown and displaying in red, correct answers display green. updated readme, slight css changes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Code-Quiz
 
 Start Button will start a timer that will display game over when there is no time remaining (reaches 0)
+
+Incorrect answer will take an additional 9 seconds off of the countdown with the next tick and display "Incorrect" in red
+Correct answer will display "Correct" in green
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,66 @@
 // when I hit start a timer starts and the quiz starts
-// when I answer a question it displays whether the answer was right or wrong
 // if answer is right score will be increased by one
 // if wrong it will shorten remaining time left on the timer
 // after quiz allow recording of scores with name and show previous scores
 
-var secondsLeft = 5
+
+//Sets the default duration of timer
+var secondsLeft = 500
 var timerDisplay = document.querySelector('.time-left')
 var startBtn = document.querySelector('#start-button')
+var questionResult = document.querySelector('.question-result')
+var question1 = document.querySelector('.question-1')
+//TO DO  DO THE SAME AS ABOVE FOR OTHER QUESTIONS
+var answer
 
+question1.style.visibility = "hidden"
+//TO DO  DO THE SAME AS ABOVE FOR OTHER QUESTIONS
 
+//TO ADD WHEN WRONG TAKES AWAY TIME FROM THE TIMER, CANNOT BE SELECTED MULTIPLE TIMES
 startBtn.addEventListener('click', function () {
     var timer = setInterval(function () {
         secondsLeft--
         timerDisplay.textContent = secondsLeft + ' Seconds Remaining';
+        startBtn.style.visibility = 'hidden'
+        question1.style.visibility = 'visible'
+        // TO DO, DO THE SAME AS ABOVE FOR OTHER QUESTIONS
+
+        // If answer wrong, it will take an additional amount of time off and states that it is incorrect
+        if (answer === 'Incorrect') {
+            secondsLeft = secondsLeft - 9
+            questionResult.textContent = 'Incorrect'
+        }
+        // sets back to counting down by one so it does not keep going down by number declared above
+        if (answer = '') {
+            secondsLeft = secondsLeft--
+        }
         if (secondsLeft < 0) {
             timerDisplay.textContent = 'Game Over'
             clearInterval(timer)
+            // TO DO DISPLAY FINISHED SCORE, HIDE EVERYTHING ELSE, ENTER AND SHOW SCORES
         }
     }, 1000)
 })
 
-var wrongAnswer = document.querySelector('.wrong-answer')
+var wrongAnswer1 = document.querySelector('.wrong-answer-1')
+var wrongAnswer2 = document.querySelector('.wrong-answer-2')
+var wrongAnswer3 = document.querySelector('.wrong-answer-3')
 var rightAnswer = document.querySelector('.correct-answer')
 
-
-wrongAnswer.addEventListener('click', function (){
-    var questionResult = document.getElementsByClassName('.question-result')
-    questionResult.textContent = 'Incorrect'
+wrongAnswer1.addEventListener('click', function (wrongAnswer) {
+    answer = 'Incorrect'
+    questionResult.setAttribute('style', 'color:red')
+})
+wrongAnswer2.addEventListener('click', function () {
+    answer = 'Incorrect'
+    questionResult.setAttribute('style', 'color:red')
+})
+wrongAnswer3.addEventListener('click', function () {
+    answer = 'Incorrect'
+    questionResult.setAttribute('style', 'color:red')
+})
+rightAnswer.addEventListener('click', function () {
+    answer = 'Correct'
+    questionResult.textContent = 'Correct'
+    questionResult.setAttribute('style', 'color:green')
 })
